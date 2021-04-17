4281c2a6facf2ef7cf90030a8e8580b9d54f42cf
Entraff <thomasvivas@icloud.com>
2021-04-17 17:50:16 -0400

    Added board templates

--- conway.cpp
+++ conway.cpp
@@ -5,9 +5,30 @@
 #include <iostream>
 #include "board.h"
 #include <string>
+#include <sstream>
+#include <unordered_map>
+
+std::unordered_map<std::string, std::string> templates = {
+    { 
+    "blinker",
+    "0 0 0 0 0\n0 0 1 0 0\n0 0 1 0 0\n0 0 1 0 0\n0 0 0 0 0"
+    }
+};
 
 void sleep(int x) {
     std::this_thread::sleep_for(std::chrono::milliseconds(x));
+}
+
+// Allows text to be read by parseLines function
+std::vector<std::string> split(const std::string& text) {
+    std::stringstream ss(text);
+    std::string current;
+    std::vector<std::string> res;
+
+    while (std::getline(ss, current, '\n')) {
+        res.push_back(current);
+    } 
+    return res;
 }
 
 // Requires config.txt file
@@ -43,12 +64,40 @@ std::vector<std::vector<bool>> parseLines(const std::vector<std::string>& text) 
     return res;
 }
 
+
+
 int main() {
-    std::vector<std::vector<bool>> initialBoard = parseLines(readFile());
+    std::vector<std::vector<bool>> initialBoard;
+    std::string templateName;
+    char mode;
+
+    std::cout << "Board Template or Custom (config.txt)?" << std::endl;
+    std::cout << "[T]emplate, [C]ustom" << std::endl;
+    std::cin >> mode;
+
+    switch (mode) {
+        case 'T':
+            std::cout << "Which board template would you like to use?" << std::endl;
+            for (auto const& [title, boardLayout] : templates) {
+                std::cout << title << std::endl;
+                std::cout << boardLayout << std::endl;
+
+            std::cin >> templateName;
+            initialBoard = parseLines(split(templates[templateName]));
+
+            break;
+        }
+
+        case 'C':
+            initialBoard = parseLines(readFile());
+            break;
+    }
+
     Board board(initialBoard);
-    
+
     std::cout << "Press enter to contine : ";
     std::cin.ignore();
+    std::cout << std::endl;
 
     while (true) {
         board.printBoard();
